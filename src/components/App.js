26b286b4aa154d3d85b0c1222150249ea43c5ce4
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-03-14 07:48:22 +0000

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,26 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+
+const App = () => {
+
+    <>
+
+    </>
+
+    // const [count, setCount] = useState(0);
+
+    // function increment() {
+    //     setCount((prev) => prev + 1)
+    // }
+
+
+    return (
+        <>
+            <div id="main">
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            </div>
+        </>
+    )
+}
+
+
 
